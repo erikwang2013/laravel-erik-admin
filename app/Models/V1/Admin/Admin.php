55299c98d2e8a6d95585cd8e6758f951f88d215d
erikwang2013 <erik@erik.xyz>
2021-06-08 13:14:16 +0800

    更新

--- app/Models/V1/Admin/Admin.php
+++ app/Models/V1/Admin/Admin.php
@@ -3,191 +3,199 @@
 namespace App\Models\V1\Admin;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory,
-	Illuminate\Database\Eloquent\Model,
-	Illuminate\Support\Facades\Hash;
+    Illuminate\Database\Eloquent\Model,
+    Illuminate\Support\Facades\Hash;
 
 class Admin extends Model
 {
     use HasFactory;
     protected $table = 'admin';
-	public $incrementing = false;
-	public $timestamps = false;
+    public $incrementing = false;
+    public $timestamps = false;
 
-	protected $casts = [
-		'id'=>'string',
-		'phone'=>'int',
-		'status' => 'int'
-	];
+    protected $casts = [
+        'id' => 'string',
+        'phone' => 'int',
+        'status' => 'int'
+    ];
 
-	protected $hidden = [
-		'password',
+    protected $hidden = [
+        'password',
         'hash',
-		'access_token'
-	];
+        'access_token'
+    ];
 
-	protected $fillable = [
-		'id',
-		'name',
-		'nick_name',
-		'hash',
-		'password',
-		'phone',
-		'email',
-		'status',
-		'access_token'
-	];
+    protected $fillable = [
+        'id',
+        'name',
+        'nick_name',
+        'hash',
+        'password',
+        'phone',
+        'email',
+        'status',
+        'access_token'
+    ];
 
-	/**
-	 * 查询校验参数
-	 *
-	 * @var array
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-14 13:31:51
-	 */
-	public function findWhere(){
-		return ['id','name','nick_name','phone','email','status','access_token'];
-	}
+    /**
+     * 查询校验参数
+     *
+     * @var array
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-14 13:31:51
+     */
+    public function findWhere()
+    {
+        return ['id', 'name', 'nick_name', 'phone', 'email', 'status', 'access_token'];
+    }
 
-	/**
-	 * 管理员详情
-	 *
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-19 11:30:02
-	 * @return void
-	 */
-	public function infoId(){
-		return $this->hasOne('App\Models\V1\Admin\AdminInfo','id','id');
-	}
+    /**
+     * 管理员详情
+     *
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-19 11:30:02
+     * @return void
+     */
+    public function infoId()
+    {
+        return $this->hasOne('App\Models\V1\Admin\AdminInfo', 'id', 'id');
+    }
 
 
-	/**
-	 * 
-	 *管理员列表
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-14 19:52:33
-	 * @param array $params
-	 * @return void
-	 */
-	public function search($params=[],$page,$limit){	
-		$page=ceil($page-1)/$limit;
-		$model=Model::where(function($model) use($params){
-			if(count($params)>0){
-				foreach($params as $k=>$v){
-					switch($k){
-						case 'id':
-							$model->where($k,(int)$v);
-							break;
-						case 'status':
-							$model->where($k,$v);
-							break;
-						default:
-						$model->where($k,'like',$v.'%');
-						break;
-					}
-				}
-			}
-		})->with('infoId');
-		$count=$model->count();
-		$result=$model->offset($page)->limit($limit)->get()->toArray();
-		foreach($result as $k=>$v){
-			$info=$v['info_id'];
-			$result[$k]=[
-				'id'=>$v['id'],
-				'name'=>$v['name'],
-				'nick_name'=>$v['nick_name'],
-				'phone'=>$v['phone'],
-				'email'=>$v['email'],
-				'status'=>[
-					'key'=>$v['status'],
-					'value'=>$v['status']?trans('admin.status_off'):trans('admin.status_on')
-				],
-				'real_name'=>$info['real_name'],
-				'sex'=>[
-					'key'=>$info['sex'],
-					'value'=>$info['sex']?trans('admin.admin_sex_man'):trans('admin.admin_sex_woman'),
-				],
-				'age'=>$info['age']==0?date("Y")-$info['year']:$info['age'],
-				'birth'=>$info['year'].'-'.$info['month'].'-'.$info['day'],
-				'img'=>$info['img'],
-				'create_time'=>$info['create_time'],
-				'update_time'=>$info['update_time']
-			];
-			unset($v['info_id']);
-		}
+    /**
+     *
+     *管理员列表
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-14 19:52:33
+     * @param array $params
+     * @return void
+     */
+    public function search($params = [], $page, $limit)
+    {
+        $page = ceil($page - 1) / $limit;
+        $model = $this->where(function ($model) use ($params) {
+            if (count($params) > 0) {
+                foreach ($params as $k => $v) {
+                    switch ($k) {
+                        case 'id':
+                            $model->where($k, (int)$v);
+                            break;
+                        case 'status':
+                            $model->where($k, $v);
+                            break;
+                        default:
+                            $model->where($k, 'like', $v . '%');
+                            break;
+                    }
+                }
+            }
+        })->with('infoId');
+        $count = $model->count();
+        $result = $model->offset($page)->limit($limit)->get()->toArray();
+        foreach ($result as $k => $v) {
+            $info = $v['info_id'];
+            $result[$k] = [
+                'id' => $v['id'],
+                'name' => $v['name'],
+                'nick_name' => $v['nick_name'],
+                'phone' => $v['phone'],
+                'email' => $v['email'],
+                'status' => [
+                    'key' => $v['status'],
+                    'value' => $v['status'] ? trans('admin.status_off') : trans('admin.status_on')
+                ],
+                'real_name' => $info['real_name'],
+                'sex' => [
+                    'key' => $info['sex'],
+                    'value' => $info['sex'] ? trans('admin.admin_sex_man') : trans('admin.admin_sex_woman'),
+                ],
+                'age' => $info['age'] == 0 ? date("Y") - $info['year'] : $info['age'],
+                'birth' => $info['year'] . '-' . $info['month'] . '-' . $info['day'],
+                'img' => $info['img'],
+                'create_time' => $info['create_time'],
+                'update_time' => $info['update_time']
+            ];
+            unset($v['info_id']);
+        }
 
-		return [
-			'list'=>$result,
-			'count'=>$count
-		];
-	}
+        return [
+            'list' => $result,
+            'count' => $count
+        ];
+    }
 
-	/**
-	 * 新增管理员
-	 *
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-19 15:00:25
-	 * @param [type] $data
-	 * @return void
-	 */
-	public function create($data){
-		return Model::create($data);
-	}
+    /**
+     * 新增管理员
+     *
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-19 15:00:25
+     * @param [type] $data
+     * @return void
+     */
+    public function create($data)
+    {
+        return $this->create($data);
+    }
 
-	/**
-	 * 更新管理员
-	 *
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-22 17:56:51
-	 * @param [type] $data
-	 * @param [type] $id
-	 * @return void
-	 */
-	public function updateData($data,$id){
-		return Model::where('id',$id)->update($data);
-	}
+    /**
+     * 更新管理员
+     *
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-22 17:56:51
+     * @param [type] $data
+     * @param [type] $id
+     * @return void
+     */
+    public function updateData($data, $id)
+    {
+        return $this->where('id', $id)->update($data);
+    }
 
-	public function deleteAll($id){
-		return Model::whereIn('id',$id)->destroy();
-	}
-	/**
-	 * 密码加密
-	 *
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-19 16:21:07
-	 * @param [type] $password
-	 * @return void
-	 */
-	public function setPassword($password){
-		return Hash::make($password);
-	}
+    public function deleteAll($id)
+    {
+        return $this->whereIn('id', $id)->destroy();
+    }
+    /**
+     * 密码加密
+     *
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-19 16:21:07
+     * @param [type] $password
+     * @return void
+     */
+    public function setPassword($password)
+    {
+        return Hash::make($password);
+    }
 
-	/**
-	 * 校验密码
-	 *
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-20 10:45:53
-	 * @param [type] $password
-	 * @param [type] $hash
-	 * @return void
-	 */
-	public function checkPassword($password,$hash){
-		if(Hash::check($password,$hash)){
-			return true;
-		}
-		return false;
-	}
+    /**
+     * 校验密码
+     *
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-20 10:45:53
+     * @param [type] $password
+     * @param [type] $hash
+     * @return void
+     */
+    public function checkPassword($password, $hash)
+    {
+        if (Hash::check($password, $hash)) {
+            return true;
+        }
+        return false;
+    }
 }
--- app/Models/V1/Admin/AdminInfo.php
+++ app/Models/V1/Admin/AdminInfo.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models\V1\Admin;
 
 use Illuminate\Database\Eloquent\Model;
@@ -6,72 +7,76 @@ use Illuminate\Database\Eloquent\Model;
 class AdminInfo extends Model
 {
     protected $table = 'admin_info';
-	public $incrementing = false;
-	public $timestamps = false;
+    public $incrementing = false;
+    public $timestamps = false;
 
-	protected $casts = [
-		'id'=>'string',
-		'sex' => 'int',
-		'age'=>'int',
-		'year'=>'int',
-        'month'=>'int',
-        'day'=>'int',
-	];
+    protected $casts = [
+        'id' => 'string',
+        'sex' => 'int',
+        'age' => 'int',
+        'year' => 'int',
+        'month' => 'int',
+        'day' => 'int',
+    ];
 
-	protected $hidden = [];
+    protected $hidden = [];
 
-	protected $fillable = [
-		'id',
-		'real_name',
-		'img',
-		'create_time',
-		'update_time',
-	];
+    protected $fillable = [
+        'id',
+        'real_name',
+        'img',
+        'create_time',
+        'update_time',
+    ];
 
-	/**
-	 * 查询校验参数
-	 *
-	 * @var array
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-14 13:31:51
-	 */
-	public function findWhere(){
-		return ['id','real_name','sex','age','year','month','day','img','create_time','update_time'];
-	}
+    /**
+     * 查询校验参数
+     *
+     * @var array
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-14 13:31:51
+     */
+    public function findWhere()
+    {
+        return ['id', 'real_name', 'sex', 'age', 'year', 'month', 'day', 'img', 'create_time', 'update_time'];
+    }
 
-	/**
-	 * 新增管理员详情
-	 *
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-19 15:26:20
-	 * @param [type] $data
-	 * @return void
-	 */
-	public function create($data){
-		$result=Model::create($data);
-		return $result;
-	}
+    /**
+     * 新增管理员详情
+     *
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-19 15:26:20
+     * @param [type] $data
+     * @return void
+     */
+    public function create($data)
+    {
+        $result = $this->create($data);
+        return $result;
+    }
 
-	/**
-	 * 更新管理员详情
-	 *
-	 * @Author erik
-	 * @Email erik@erik.xyz
-	 * @Url https://erik.xyz
-	 * @DateTime 2021-04-22 17:59:16
-	 * @param [type] $data
-	 * @param [type] $id
-	 * @return void
-	 */
-	public function updateData($data,$id){
-		return Model::where('id',$id)->update($data);
-	}
+    /**
+     * 更新管理员详情
+     *
+     * @Author erik
+     * @Email erik@erik.xyz
+     * @Url https://erik.xyz
+     * @DateTime 2021-04-22 17:59:16
+     * @param [type] $data
+     * @param [type] $id
+     * @return void
+     */
+    public function updateData($data, $id)
+    {
+        return $this->where('id', $id)->update($data);
+    }
 
-	public function deleteAll($id){
-		return Model::whereIn('id',$id)->destroy();
-	}
+    public function deleteAll($id)
+    {
+        return $this->whereIn('id', $id)->destroy();
+    }
 }
