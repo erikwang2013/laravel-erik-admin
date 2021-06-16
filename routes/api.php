e57e9ca0944766efbb03b2bbc8a74989a677c0cd
erikwang2013 <erik@erik.xyz>
2021-06-16 20:24:50 +0800

    更新

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,12 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['prefix' => 'v1'], function () {
-    //后台接口
-    Route::group(['prefix' => 'backstage'], function () {
-        Route::get('/admin', 'V1\Backstage\AdminController@index');  //管理员列表
-        Route::post('/admin', 'V1\Backstage\AdminController@store'); //新增管理员
-        Route::put('/admin', 'V1\Backstage\AdminController@update'); //更新管理员
-        Route::delete('/admin', 'V1\Backstage\AdminController@destroy'); //删除管理员
+
+Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
+    Route::group(['prefix' => 'v1'], function () {
+        //后台接口
+        Route::group(['prefix' => 'backstage'], function () {
+            Route::get('/admin', 'V1\Backstage\AdminController@index');  //管理员列表
+            Route::post('/admin', 'V1\Backstage\AdminController@store'); //新增管理员
+            Route::put('/admin', 'V1\Backstage\AdminController@update'); //更新管理员
+            Route::delete('/admin', 'V1\Backstage\AdminController@destroy'); //删除管理员
+        });
     });
 });
