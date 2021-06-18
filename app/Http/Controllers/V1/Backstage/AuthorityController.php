3f6caa00a012f4fccdbf276a6f3597a0bf38fb97
erikwang2013 <erik@erik.xyz>
2021-06-18 13:26:02 +0800

    更新

--- app/Http/Controllers/V1/Backstage/AuthorityController.php
+++ app/Http/Controllers/V1/Backstage/AuthorityController.php
@@ -6,12 +6,30 @@ use App\Http\Controllers\Controller,
     App\Support\Facades\V1\Models\BaseValidationFacade,
     App\Common\HelperCommon,
     Illuminate\Http\Request,
+    Illuminate\Support\Facades\Validator,
     App\Support\Facades\V1\Services\AuthorityServiceFacade;
 
 class AuthorityController extends Controller
 {
     public function index(Request $request)
     {
+        $params = $request->input();
+        //校验数据
+        if (!BaseValidationFacade::validateRequest($request, 'index')) {
+            return HelperCommon::reset([], 0, 1, BaseValidationFacade::getError());
+        }
+        //验证分页
+        $pageData = [
+            'page' => isset($params['page']) ? $params['page'] : config('app.page'),
+            'limit' => isset($params['limit']) ? $params['limit'] : config('app.limit')
+        ];
+        if (!BaseValidationFacade::check($pageData)) {
+            return HelperCommon::reset([], 0, 1, BaseValidationFacade::getError());
+        }
+
+        //过滤存在的数据
+        $data = HelperCommon::filterKey(AdminFacade::class, $params, 0);
+        return AuthorityServiceFacade::index($data, $pageData);
     }
 
     public function store(Request $request)
@@ -31,11 +49,27 @@ class AuthorityController extends Controller
         return AuthorityServiceFacade::store($data, $params);
     }
 
-    public function update()
+    public function update(Request $request, $id)
     {
+        $params = $request->input();
+        $params['id'] = $id;
+        //校验数据
+        if (!BaseValidationFacade::validateRequest($request, 'update')) {
+            return HelperCommon::reset([], 0, 1, BaseValidationFacade::getError());
+        }
+        unset($params['id']);
+        return AuthorityServiceFacade::update($params, $id);
     }
 
-    public function destroy()
+    public function destroy($id)
     {
+        $ids = explode(',', $id);
+        foreach ($ids as $k => $v) {
+            $validator = Validator::make(['id' => $v], ['id' => 'numeric|min:19|required']);
+            if ($validator->fails()) {
+                return HelperCommon::reset([], 0, 1, $validator->errors());
+            }
+        }
+        return AuthorityServiceFacade::destroy($ids);
     }
 }
---
+++ app/Http/Validations/V1/Backstage/AuthorityValidation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Validations\V1\Backstage;
+
+class AuthorityValidation
+{
+    public function index()
+    {
+        return [
+            'rules' => [],
+            'messages' => []
+        ];
+    }
+
+    public function store()
+    {
+        return [
+            'rules' => [],
+            'messages' => []
+        ];
+    }
+
+    public function update()
+    {
+        return [
+            'rules' => [],
+            'messages' => []
+        ];
+    }
+}
